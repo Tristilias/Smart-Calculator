7e1181d90bcf8cf800e05a4ca76e28e4380904bc
tristilias <adisramdhani98@gmail.com>
2026-05-02 12:53:47 +0800

    fix: fixing dasar.cpp variable

--- dasar.cpp
+++ dasar.cpp
@@ -2,16 +2,16 @@
 #include "dasar.h"
 using namespace std;
 
-double Penjumlahan(double a, double b) {
+double Tambah(double a, double b) {
     return a + b;
 }
-double Pengurangan(double a, double b) {
+double Kurang(double a, double b) {
     return a - b;
 }
-double Perkalian(double a, double b) {
+double Kali(double a, double b) {
     return a * b;
 }
-double Pembagian(double a, double b) {
+double Bagi(double a, double b) {
     if (b != 0) {
         return a / b;
     } else {
@@ -48,16 +48,16 @@ void menuDasar() {
 
     switch (pilihan) {
         case 1:
-            cout << "Hasil Penjumlahan: " << Penjumlahan(a, b) << endl;
+            cout << "Hasil Penjumlahan: " << Tambah(a, b) << endl;
             break;
         case 2:
-            cout << "Hasil Pengurangan: " << Pengurangan(a, b) << endl;
+            cout << "Hasil Pengurangan: " << Kurang(a, b) << endl;
             break;
         case 3:
-            cout << "Hasil Perkalian: " << Perkalian(a, b) << endl;
+            cout << "Hasil Perkalian: " << Kali(a, b) << endl;
             break;
         case 4:
-            cout << "Hasil Pembagian: " << Pembagian(a, b) << endl;
+            cout << "Hasil Pembagian: " << Bagi(a, b) << endl;
             break;
         case 5:
             cout << "Hasil Modulus: " << Modulus(a, b) << endl;
